1c927bfb73296b7ce27e30e16bfa4ef9d19abc83
daphnemillss <dafnymastrelly@gmail.com>
2018-08-25 18:34:21 -0300

    classe desvio padrao e teste

---
+++ src/main/java/desviopadrao/DesvioPadrao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desviopadrao;
+
+/**
+ *
+ * @author daphnemillss
+ */
+public class DesvioPadrao {
+    public double CalculaDesvioPadrao(double v[]){
+        return 1.414214;
+    }
+}
