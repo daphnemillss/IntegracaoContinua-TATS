2b105ad2f93d9f8c9a7e4b0bc93a2e69c62f31db
daphnemillss <dafnymastrelly@gmail.com>
2018-08-25 18:48:46 -0300

    Maior Menor Teste

---
+++ src/main/java/maiormenor/NMaiorNMenor.java
@@ -0,0 +1,21 @@
+package maiormenor;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author daphnemillss
+ */
+public class NMaiorNMenor {
+    double[] getNMaior(double[] v, int i){
+        double vet[] = {4, 5};
+        return vet;
+    }
+    double[] getNMenor(double[] v, int i){
+        double vet[] = {1,2};
+        return vet;
+    }
+}
